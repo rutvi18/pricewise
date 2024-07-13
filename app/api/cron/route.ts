51a163edc84e2a149e080b148e1b50d18a88debe
Rutvi <rutvi@Rutvis-MacBook-Air.local>
2024-07-13 22:56:09 +0530

    first commit

---
+++ app/api/cron/route.ts
@@ -0,0 +1,67 @@
+import Product from "@/lib/models/product.models";
+import { connectToDB } from "@/lib/mongoose"
+import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
+import { scrapeAmazonProduct } from "@/lib/scraper";
+import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try{
+        connectToDB();
+
+        const products = await Product.find({});
+
+        if(!products) throw new Error("No products found");
+
+        //1. Scrape latest product details and update db
+
+        const updatedProducts = await Promise.all(
+            products.map(async (currentProduct) => {
+                const scrapedProduct = await scrapeAmazonProduct(currentProduct.url);
+
+                if(!scrapedProduct) throw new Error("No product found");
+
+                const updatedPriceHistory = [
+                    ...currentProduct.priceHistory,
+                    { price: scrapedProduct.currentPrice }
+                ]
+
+                const product = {
+                    ...scrapedProduct,
+                    priceHistory: updatedPriceHistory,
+                    lowestPrice: getLowestPrice(updatedPriceHistory),
+                    highestPrice: getHighestPrice(updatedPriceHistory),
+                    averagePrice: getAveragePrice(updatedPriceHistory),
+                }
+                const updatedProduct = await Product.findOneAndUpdate(
+                    { url: scrapedProduct.url },
+                    product,
+                );
+
+                //Check each product status and email
+                const emailNotifType = getEmailNotifType(scrapedProduct, currentProduct)
+
+                if(emailNotifType && updatedProduct.users.length > 0){
+                    const productInfo = {
+                        title: updatedProduct.title,
+                        url: updatedProduct.url,
+                    }
+
+                    const emailContent = await generateEmailBody(productInfo, emailNotifType);
+
+                    const userEmails = updatedProduct.users.map((user: any) => user.email)
+
+                    await sendEmail(emailContent, userEmails);
+                }           
+
+                return updatedProduct;
+            })
+        )
+
+        return NextResponse.json({
+            message: 'Ok', data: updatedProducts
+        })
+    } catch(error){
+        throw new Error(`Error in GET: ${error}`)
+    }
+}
---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** 
+ * @type {import('next').NextConfig} 
+ * */
+const nextConfig = {
+    experimental: {
+      serverComponentsExternalPackages: ['mongoose']
+    },
+    images: {
+      domains: ['m.media-amazon.com']
+    }
+  }
+  
+  module.exports = nextConfig
